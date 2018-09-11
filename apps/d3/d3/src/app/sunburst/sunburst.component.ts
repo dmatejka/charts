94b4546bbb37a23ed2c38811c27bee0940e82277
dmatejka <matejka@xpression.sk>
2018-09-11 18:18:17 +0200

    labels

--- apps/d3/d3/src/app/sunburst/sunburst.component.ts
+++ apps/d3/d3/src/app/sunburst/sunburst.component.ts
@@ -3,7 +3,6 @@ import * as d3 from 'd3';
 import * as ds from '../data.service';
 // import { tap } from 'rxjs/operators';
 
-
 @Component({
   selector: 'charts-sunburst',
   templateUrl: './sunburst.component.html',
@@ -23,12 +22,9 @@ export class SunburstComponent implements OnInit {
   private width: number;
   private xScale: any;
   private yScale: any;
-  // public data: any;
 
-  constructor( public d: ds.DataService) { }
-  @ViewChild('chart')
-  private chartContainer: ElementRef;
-
+  constructor(public data: ds.DataService) {}
+  @ViewChild('chart') private chartContainer: ElementRef;
 
   /**
    * mouseoverArc: shows tooltip dive when mouse moves over arc.
@@ -38,9 +34,10 @@ export class SunburstComponent implements OnInit {
    */
   mouseoverArc(d: any): any {
     this.tooltip.html(this.tooltipHTML(d));
-    return this.tooltip.transition()
-        .duration(50)
-        .style('opacity', 0.9);
+    return this.tooltip
+      .transition()
+      .duration(50)
+      .style('opacity', 0.9);
   }
 
   /**
@@ -57,8 +54,21 @@ export class SunburstComponent implements OnInit {
    */
   mousemoveArc(): any {
     return this.tooltip
-        .style('top', (d3.event.pageY - 10) + 'px')
-        .style('left', (d3.event.pageX + 10) + 'px');
+      .style('top', d3.event.pageY - 10 + 'px')
+      .style('left', d3.event.pageX + 10 + 'px');
+  }
+
+  /**
+   * computeTextRotation: Modify the orientation of category labels.
+   * @returns Number
+   */
+  //
+  computeTextRotation(d): Number {
+    const angle = (d.x0 + d.x1) * 180;
+
+    // Avoid upside-down labels; labels as rims
+    return angle < 120 || angle > 270 ? angle : angle + 180;
+    // return (angle < 180) ? angle - 90 : angle + 90;  // labels as spokes
   }
 
   ngOnInit() {
@@ -66,24 +76,31 @@ export class SunburstComponent implements OnInit {
 
     this.margin = 10;
     this.createChart();
-    this.updateChart(this.d.data);
+    this.updateChart(this.data.data);
   }
 
+  // Creating base for chart
   private createChart(): void {
     const element = this.chartContainer.nativeElement;
     this.width = element.offsetWidth - this.margin * 2;
     this.height = element.offsetHeight - this.margin * 2;
-    this.radius = (Math.min(this.width, this.height) / 2) - this.margin * 2
+    this.radius = Math.min(this.width, this.height) / 2 - this.margin * 2;
 
     /* create svg */
-    this.svg = d3.select(element).append('svg')
+    this.svg = d3
+      .select(element)
+      .append('svg')
       .attr('width', element.offsetWidth)
       .attr('height', element.offsetHeight)
       .append('g')
-      .attr('transform', 'translate(' + this.width / 2 + ',' + this.height / 2 + ') rotate(-90 0 0)');
+      .attr(
+        'transform',
+        'translate(' + this.width / 2 + ',' + this.height / 2 + ')'
+      );
 
     /* create tooltip div */
-    this.tooltip = d3.select(element)
+    this.tooltip = d3
+      .select(element)
       .append('div')
       .attr('id', 'tooltip')
       .style('position', 'absolute')
@@ -93,65 +110,79 @@ export class SunburstComponent implements OnInit {
       .style('padding', '5px 10px');
 
     /* set up scales */
-    this.xScale = d3.scaleLinear()
-      .range([0, 2 * Math.PI]);
-    this.yScale = d3.scaleSqrt()
-      .range([0, this.radius]);
-    this.color = d3.scaleOrdinal(d3.schemeCategory20c);
+    this.xScale = d3.scaleLinear().range([0, 2 * Math.PI]);
+    this.yScale = d3.scaleLinear().range([0, this.radius]);
+    this.color = d3.scaleOrdinal(d3.schemeBlues[9]);
 
     /* layout */
     this.partition = d3.partition();
 
     /* arc function for calculating slices of sunburst */
-    this.arc = d3.arc()
-      // .startAngle((d: any) => Math.max(0, Math.min(2 * Math.PI, this.xScale(d['x0']))))
-      // .endAngle((d: any) => Math.max(0, Math.min(2 * Math.PI, this.xScale(d['x1']))))
-      .startAngle((d: any) => Math.max(0, Math.min(2 * Math.PI, this.xScale(d.x0))))
-      .endAngle((d: any) => Math.max(0, Math.min(2 * Math.PI, this.xScale(d.x1))))
+    this.arc = d3
+      .arc()
+      .startAngle((d: any) =>
+        Math.max(0, Math.min(2 * Math.PI, this.xScale(d['x0'])))
+      )
+      .endAngle((d: any) =>
+        Math.max(0, Math.min(2 * Math.PI, this.xScale(d['x1'])))
+      )
       .innerRadius((d: any) => Math.max(0, this.yScale(d['y0'])))
       .outerRadius((d: any) => Math.max(0, this.yScale(d['y1'])));
 
     this.created = true;
   }
 
-
-
   private updateChart(tree: any): void {
     this.root = d3.hierarchy(tree);
-    console.log('tree:', tree)
-    console.log('root:', this.root)
-    this.root.count(function(d) { return d.value; });
+    this.root.count(d => d.value); // sunburst portions based on number of elements
+    // this.root.sum(d => d.value); // sunburst portions based on sum of element's value
 
     /* JOIN new data with old elements */
-    const path = this.svg.selectAll('path')
-    .data(this.partition(this.root).descendants());
-    console.log('path:', path)
+    const path = this.svg
+      .selectAll('g')
+      .data(this.partition(this.root).descendants())
+      .enter()
+      .append('g');
 
     /* ENTER new elements present in new data */
     /* UPDATE old elements present in new data */
-    path.enter()
+    path
       .append('path')
-      .on('mouseover', (d) => {console.log(d); return this.mouseoverArc(d)})
-      .on('mousemove', (d) => this.mousemoveArc())
-      .on('mouseout',  (d) => this.mouseoutArc())
+      .on('mouseover', d => this.mouseoverArc(d))
+      .on('mousemove', d => this.mousemoveArc())
+      .on('mouseout', d => this.mouseoutArc())
       // .on('click',     (d) => this.clickArc(d))
-      .merge(path)      // ENTER + UPDATE
-        .transition().duration(500)
-        .attr('d', this.arc)
-        .style('stroke', '#ffffff')
-        .style('fill', 'blue')
-        // .style('cursor', (d: any) => d.data.level === 'parkName' || d.data.level === 'type' ? 'pointer' : 'auto');
+      .merge(path) // ENTER + UPDATE
+      .transition()
+      .duration(500)
+      .attr('d', this.arc)
+      .style('stroke', '#ffffff')
+      .style('fill', (d: any) => d.data.color ? d.data.color : this.color(d.depth))
+      .attr('class', 'node');
+
+    path
+      .append('text')
+      .attr('text-anchor', 'middle')
+      .attr('transform', d => {
+        return d.depth === 0
+          ? ''
+          : 'translate(' +
+              this.arc.centroid(d) +
+              ') rotate(' +
+              this.computeTextRotation(d) +
+              ')';
+      })
+      .text(d =>  d.data.name );
 
     /* EXIT old elements not present in new data */
-    path.exit()
+    path
+      .exit()
       .transition()
       .duration(500)
       .remove();
   }
 
-
-
   private tooltipHTML(d: any): string {
-    return  `${d.data.name} / <strong>${d.data.value}</strong>`;
+    return `${d.data.name}`;
   }
 }
